291a29320f7fa83f7345ba364e48760c31088303
tkiyotsune <tkiyotsune@gmail.com>
2020-10-29 02:00:09 +0900

    テストの勉強

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+use src\Sample;
+
+class index{
+
+    echo Sample::returnHello;
+
+}
---
+++ src/Sample.php
@@ -0,0 +1,10 @@
+<?php
+namespace src;
+
+class Sample
+{
+    static public function returnHello()
+    {
+        return "hello";
+    }
+}
